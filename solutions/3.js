12713bd90c50ff54cf9a6697eb7e471c43b9fb1f
Stefan Temme <temme@objectcode.de>
2021-05-27 23:11:53 +0200

    cleanup

--- solutions/3.js
+++ solutions/3.js
@@ -1,14 +1,10 @@
-import {factorization, eratosthenes} from "../reusable/primes.js";
+﻿import {factorization} from "../reusable/primes.js";
 
 console.log('Largest prime factor\n');
 console.log(`The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?`);
 console.log('https://projecteuler.net/problem=3\n');
 
-/**
- *
- *
- */
 
 const upper = 600851475143;
 const facts = factorization(upper);
