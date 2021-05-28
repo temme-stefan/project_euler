e5e066bee2a250488428d271ffef0369e52e5124
Stefan Temme <temme@objectcode.de>
2021-05-28 23:49:58 +0200

    Lösung 15

---
+++ solutions/15.js
@@ -0,0 +1,17 @@
+console.log('Lattice paths\n');
+console.log(`Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+How many such routes are there through a 20×20 grid?`);
+console.log('https://projecteuler.net/problem=1\n');
+
+
+/**
+ * ( 40 )
+ * (    ) = 137846528820
+ * ( 20
+ *
+ * https://www.arndt-bruenner.de/mathe/scripts/binkoeff1.htm
+ */
+
+
+
+console.log("Solution:", 137846528820 );
