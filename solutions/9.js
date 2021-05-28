4d42e1b320442340c0707439ffdbfb417aaa3de0
Stefan Temme <temme@objectcode.de>
2021-05-28 10:52:25 +0200

    Solution 9

--- solutions/9.js
+++ solutions/9.js
@@ -9,18 +9,18 @@ There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.`);
 console.log('https://projecteuler.net/problem=9\n');
 
-const n=1000;
-let result;
-for (let a=1;result==null && a<n;a++){
-    const a2=a*a;
-    for (let b=a+1;result==null && b<n-a;b++){
-        const b2=b*b;
-        const a2b2=a2+b2;
-        const c=n-a-b;
-        const c2=c*c;
-        if (c>b && a2b2==c2){
-            result=a*b*c;
-            console.log(a,b,c);
+const n = 1000;
+let result = null;
+for (let a = 1; result == null && a < n; a++) {
+    const a2 = a * a;
+    for (let b = a + 1; result == null && b < n - a; b++) {
+        const b2 = b * b;
+        const a2b2 = a2 + b2;
+        const c = n - a - b;
+        const c2 = c * c;
+        if (c > b && a2b2 === c2) {
+            result = a * b * c;
+            console.log(a, b, c);
         }
 
     }
