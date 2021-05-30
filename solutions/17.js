f8785bef3c5397d7fa74047c567165216287e2ef
Stefan Temme <temme@objectcode.de>
2021-05-30 13:38:49 +0200

    Solution 17

--- solutions/17.js
+++ solutions/17.js
@@ -1,5 +1,3 @@
-import {toNumberArray} from "../reusable/numberArrayArithmetics.js";
-
 console.log("Number letter counts");
 console.log(`If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
 If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
@@ -9,64 +7,59 @@ console.log('https://projecteuler.net/problem=17\n');
 const firstdigit = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fiveteen", "sixteen", "seventeen", "eightteen", "nineteen"]
 const seconddigit = [, , "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 
-const getHundred = (n)=>{
-    if (n<20){
-        return firstdigit[n];
-    }
-    else{
-        const x = toNumberArray(n);
-        let s="";
-        if (x.length==3){
-            s = firstdigit[x[0]]+" hundred"+(n%100>0?" and":"");
-            x.shift();
+const getTen = (n) => {
+    let s = "";
+    if (n < 20) {
+        s += firstdigit[n]
+    } else {
+        const sec = Math.floor(n / 10);
+        const first = n % 10;
+        s = s + (seconddigit[sec] ?? "");
+        if (first > 0) {
+            s = s + firstdigit[first];
         }
-        if (x.length==2){
-            if (x[0]*10+x[1]<20){
-                s = s + firstdigit[x[0]*10+x[1]];
-            }
-            else{
-
-            }
-            s = firstdigit[x[0]]+" hundred"+(n%100>0?" and":"");
-            x.shift();
-        }
-        if (x.length==2 && x[0]*10+x[1]<20 || x.length==1){
-            s = s + firstdigit[x[0]*10+x[1]];
-        }
-        else {
-
-            s = s + (seconddigit[x[0]] ?? "");
-            if (x[1] > 0)
-                s = s + firstdigit[x[1]];
-        }
-        return s;
-    }
-
-
-    if (x.length>2){
-        s = firstdigit[x[2]]+" hundred and ";
-    }
-    if (x[1]<3){
-        s = s + firstdigit[x[1]*10+x[0]];
-    }
-    else{
     }
     return s;
 }
 
-const to = 125;
-
-for (let i = 90; i <= to; i++) {
-    let current = i;
+const getHundred = (n) => {
     let s = "";
-    while (current > 0) {
-        const step = current % 1000;
-        s= s + getHundred(step);
-        current = Math.floor(step / 1000);
+    if (n > 99) {
+        s = firstdigit[Math.floor(n / 100)] + " hundred "
+        n = n % 100;
+        if (n > 0) {
+            s = s + "and ";
+        }
     }
-    console.log(i,s);
+    if (n > 0) {
+        s = s + getTen(n);
+    }
+    return s;
+}
+
+const getThousand = (n) => {
+    let s = "";
+    const upper = Math.floor(n / 1000);
+    const lower = n % 1000;
+    if (upper > 0) {
+        s = getHundred(upper) + " thousand "
+    }
+    if (lower > 0 && n != 0) {
+        s = s + getHundred(lower);
+    }
+    return s;
 }
 
 
-const result = null;
+const to = 1000;
+
+let sum = 0;
+for (let i = 1; i <= to; i++) {
+    let s = getThousand(i);
+    s= s.split(/\W/).join('');
+    sum+=s.length;
+}
+
+
+const result = sum;
 console.log(`Solution: ${result}`);
