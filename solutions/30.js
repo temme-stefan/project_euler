9921069ce18bd02771331c69c7a8dd305a03ae66
Stefan Temme <temme@objectcode.de>
2021-06-02 22:44:58 +0200

    Solution 30 (to be improved)

---
+++ solutions/30.js
@@ -0,0 +1,54 @@
+import {toNumberArray} from "../reusable/numberArrayArithmetics.js";
+import {numberSort} from "../reusable/myMath.js";
+
+console.log("Digit fifth powers");
+console.log(`Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+
+1634 = 1^4 + 6^4 + 3^4 + 4^4
+8208 = 8^4 + 2^4 + 0^4 + 8^4
+9474 = 9^4 + 4^4 + 7^4 + 4^4
+As 1 = 1^4 is not a sum it is not included.
+
+The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+
+Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.`);
+console.log('https://projecteuler.net/problem=30\n');
+
+const power = 5;
+const numbers =[]
+const maxNumber = power * Math.pow(9,power);
+
+const lookup=new Map();
+
+const digitPowerSum = (n)=>{
+    let s=toNumberArray(n);
+    s.sort(numberSort);
+    let key = s.reduce((a,b)=>a*10+b,0);
+    if (!lookup.has(key)){
+        if (n<10){
+            lookup.set(key,Math.pow(n,power));
+        }
+        else{
+            const firstHalf=s.filter((x,i)=>i<s.length/2).reduce((a,b)=>a*10+b,0);
+            const secondHalf=s.filter((x,i)=>i>=s.length/2).reduce((a,b)=>a*10+b,0);
+            lookup.set(key,digitPowerSum(firstHalf)+digitPowerSum(secondHalf))
+        }
+    }
+    return lookup.get(key);
+}
+
+
+const isDigitPower= (n)=>{
+  return digitPowerSum(n)==n;
+};
+
+for (let i=2;i<maxNumber;i++){
+    if (isDigitPower(i)){
+        numbers.push(i);
+    }
+}
+
+
+const result = numbers.reduce((a,b)=>a+b,0);
+console.log("Solution:", result);
+
