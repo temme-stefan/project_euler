b62edf77e78c289750a801e06f158c53267f1a4e
Stefan Temme <temme@objectcode.de>
2021-06-04 20:00:42 +0200

    init

---
+++ init.js
@@ -0,0 +1,52 @@
+import fs from 'fs';
+import readline from 'readline';
+import {fileURLToPath} from 'url';
+import {dirname} from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+let {2: problemNumber} = process.argv;
+problemNumber = parseInt(problemNumber);
+
+if (problemNumber == null || !Number.isFinite(problemNumber)) {
+    error("no valid problemNumber given");
+}
+const h = Math.floor(problemNumber / 100);
+const t = Math.floor(problemNumber % 100 / 10);
+
+const path = `solutions/${h}00-${h}99/${h}${t}0-${h}${t}9/${problemNumber}.js`
+const file = __dirname + "/" + path;
+const dir = dirname(file);
+
+if (fs.existsSync(file)) {
+    error("File already exists: " + path);
+}
+if (!fs.existsSync(dir) ){
+   fs.mkdirSync(dir,{recursive:true});
+}
+
+const stream = fs.createWriteStream(file);
+stream.write(
+    `console.log("Title");
+console.log(\`Description\`);
+console.log("https://projecteuler.net/problem=${problemNumber}");
+
+
+
+const result = null;
+console.log("Solution: ",result);
+`
+);
+stream.end();
+
+console.log(`${path} created
+to run type:
+node ${path}
+`);
+
+
+function error(text) {
+    throw new Error(text)
+}
---
+++ solutions/000-099/040-049/41.js
@@ -0,0 +1,10 @@
+console.log("Pandigital prime");
+console.log(`We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital and is also prime.
+
+What is the largest n-digit pandigital prime that exists?`);
+console.log("https://projecteuler.net/problem=41");
+
+
+
+const result = null;
+console.log("Solution: ",result);
